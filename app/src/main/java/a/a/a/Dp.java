Sketchware-Pro/Sketchware-Pro

feat: Add Kotlin support (Android 8+)

app/src/main/java/a/a/a/Dp.java
@@ -44,6 +44,8 @@ import mod.agus.jcoderz.editor.manage.library.locallibrary.ManageLocalLibrary;
 import mod.agus.jcoderz.lib.FilePathUtil;
 import mod.agus.jcoderz.lib.FileUtil;
 import mod.hey.studios.build.BuildSettings;
+import mod.hey.studios.compiler.kotlin.KotlinCompiler;
+import mod.hey.studios.compiler.kotlin.KotlinCompilerUtil;
 import mod.hey.studios.project.ProjectSettings;
 import mod.hey.studios.project.proguard.ProguardHandler;
 import mod.hey.studios.util.SystemLogPrinter;
@@ -302,6 +304,15 @@ public class Dp {
     public final String d() {
         StringBuilder classpath = new StringBuilder();
 
+        /*
+         * Add yq#u (.sketchware/mysc/xxx/bin/classes) if it exists
+         * since there might be compiled Kotlin files for ecj to use classpath as.
+         */
+        if (FileUtil.isExistFile(f.u)) {
+            classpath.append(f.u);
+            classpath.append(":");
+        }
+
         /* Add android.jar */
         classpath.append(o);
 
@@ -557,6 +568,13 @@ public class Dp {
             }
         }
         return extraPackages + mll.getPackageNameLocalLibrary();
+    }
+
+    /**
+     * Run kotlinc to compile Kotlin files.
+     */
+    public void compileKotlin() throws Throwable {
+        new KotlinCompiler(this).compile();
     }
 
     /**
@@ -915,6 +933,12 @@ public class Dp {
         if (f.N.p) {
             n.a("okhttp-3.9.1");
         }
+
+        // Add kotlin-stdlib if any .kt files are present in the project
+        if (KotlinCompilerUtil.areAnyKtFilesPresent(this)) {
+            n.a("kotlin-stdlib-1.3.50");
+        }
+
         ExtLibSelected.addUsedDependencies(f.N.x, n);
     }
 

app/src/main/java/com/besome/sketch/design/DesignActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Bundle;
 import android.view.Menu;
@@ -93,6 +94,7 @@ import mod.hey.studios.activity.managers.java.ManageJavaActivity;
 import mod.hey.studios.activity.managers.nativelib.ManageNativelibsActivity;
 import mod.hey.studios.build.BuildSettings;
 import mod.hey.studios.build.BuildSettingsDialog;
+import mod.hey.studios.compiler.kotlin.KotlinCompilerUtil;
 import mod.hey.studios.project.DesignActRunnable;
 import mod.hey.studios.project.custom_blocks.CustomBlocksDialog;
 import mod.hey.studios.project.proguard.ManageProguardActivity;
@@ -363,6 +365,23 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
     public void onClick(View v) {
         if (!mB.a()) {
             if (v.getId() == Resources.id.btn_execute) {
+
+                if (Build.VERSION.SDK_INT < 26 && KotlinCompilerUtil.areAnyKtFilesPresent(q)) {
+                    aB dialog = new aB(this);
+                    dialog.a(0x7f0701e5);
+                    dialog.b("Warning");
+                    dialog.a("It would seem that you have some Kotlin files in your project but your " +
+                            "Android version isn't compatible with kotlinc (which only works on devices " +
+                            "with Android 8 and higher)." +
+                            "\n\n" +
+                            "Please delete those to proceed with the compilation.");
+                    dialog.b(xB.b().a(getApplicationContext(), Resources.string.common_word_ok),
+                            Helper.getDialogDismissListener(dialog));
+                    dialog.show();
+
+                    return;
+                }
+
                 new DesignActivity.a(getApplicationContext()).execute();
             } else if (v.getId() == Resources.id.btn_compiler_opt) {
                 PopupMenu popupMenu = new PopupMenu(this, findViewById(Resources.id.btn_compiler_opt));
@@ -1119,6 +1138,15 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
                     if (d) {
                         cancel(true);
                         return;
+                    }
+
+                    if (KotlinCompilerUtil.areAnyKtFilesPresent(mDp)) {
+                        publishProgress("Kotlin is compiling...");
+                        mDp.compileKotlin();
+                        if (d) {
+                            cancel(true);
+                            return;
+                        }
                     }
 
                     publishProgress("Java is compiling...");

Khaledhassan12/TAJ-Studio

app/src/main/java/a/a/a/Dp.java
@@ -55,6 +55,8 @@ import mod.agus.jcoderz.editor.manage.library.locallibrary.ManageLocalLibrary;
 import mod.agus.jcoderz.lib.FilePathUtil;
 import mod.agus.jcoderz.lib.FileUtil;
 import mod.hey.studios.build.BuildSettings;
+import mod.hey.studios.compiler.kotlin.KotlinCompiler;
+import mod.hey.studios.compiler.kotlin.KotlinCompilerUtil;
 import mod.hey.studios.project.ProjectSettings;
 import mod.hey.studios.project.proguard.ProguardHandler;
 import mod.hey.studios.util.SystemLogPrinter;
@@ -267,6 +269,15 @@ public class Dp {
      */
     public final String d() {
         StringBuilder classpath = new StringBuilder();
+
+        /*
+         * Add yq#u (.sketchware/mysc/xxx/bin/classes) if it exists
+         * since there might be compiled Kotlin files for ecj to use classpath as.
+         */
+        if (FileUtil.isExistFile(f.u)) {
+            classpath.append(f.u);
+            classpath.append(":");
+        }
 
         /* Add android.jar */
         classpath.append(androidJarPath);
@@ -484,6 +495,13 @@ public class Dp {
             }
         }
         return extraPackages + mll.getPackageNameLocalLibrary();
+    }
+
+    /**
+     * Run kotlinc to compile Kotlin files.
+     */
+    public void compileKotlin() throws Throwable {
+        new KotlinCompiler(this).compile();
     }
 
     /**
@@ -865,6 +883,12 @@ public class Dp {
         if (yq.N.isDynamicLinkUsed) {
             builtInLibraryManager.a(BuiltInLibraries.FIREBASE_DYNAMIC_LINKS);
         }
+
+        // Add kotlin-stdlib if any .kt files are present in the project
+        if (KotlinCompilerUtil.areAnyKtFilesPresent(this)) {
+            builtInLibraryManager.a("kotlin-stdlib-1.3.50");
+        }
+
         ExtLibSelected.addUsedDependencies(yq.N.x, builtInLibraryManager);
     }
 

app/src/main/java/com/besome/sketch/design/DesignActivity.java
@@ -13,6 +13,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Build.VERSION;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
@@ -91,6 +92,7 @@ import mod.hey.studios.activity.managers.assets.ManageAssetsActivity;
 import mod.hey.studios.activity.managers.java.ManageJavaActivity;
 import mod.hey.studios.activity.managers.nativelib.ManageNativelibsActivity;
 import mod.hey.studios.build.BuildSettingsDialog;
+import mod.hey.studios.compiler.kotlin.KotlinCompilerUtil;
 import mod.hey.studios.project.DesignActRunnable;
 import mod.hey.studios.project.custom_blocks.CustomBlocksDialog;
 import mod.hey.studios.project.proguard.ManageProguardActivity;
@@ -333,6 +335,23 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
     public void onClick(View v) {
         if (!mB.a()) {
             if (v.getId() == R.id.btn_execute) {
+
+                if (Build.VERSION.SDK_INT < 26 && KotlinCompilerUtil.areAnyKtFilesPresent(q)) {
+                    aB dialog = new aB(this);
+                    dialog.a(0x7f0701e5);
+                    dialog.b("Warning");
+                    dialog.a("It would seem that you have some Kotlin files in your project but your " +
+                            "Android version isn't compatible with kotlinc (which only works on devices " +
+                            "with Android 8 and higher)." +
+                            "\n\n" +
+                            "Please delete those to proceed with the compilation.");
+                    dialog.b(xB.b().a(getApplicationContext(), R.string.common_word_ok),
+                            Helper.getDialogDismissListener(dialog));
+                    dialog.show();
+
+                    return;
+                }
+
                 new BuildAsyncTask(getApplicationContext()).execute();
             } else if (v.getId() == R.id.btn_compiler_opt) {
                 PopupMenu popupMenu = new PopupMenu(this, findViewById(R.id.btn_compiler_opt));
@@ -990,6 +1009,15 @@ public class DesignActivity extends BaseAppCompatActivity implements OnClickList
                     if (canceled) {
                         cancel(true);
                         return;
+                    }
+
+                    if (KotlinCompilerUtil.areAnyKtFilesPresent(mDp)) {
+                        publishProgress("Kotlin is compiling...");
+                        mDp.compileKotlin();
+                        if (canceled) {
+                            cancel(true);
+                            return;
+                        }
                     }
 
                     publishProgress("Java is compiling...");