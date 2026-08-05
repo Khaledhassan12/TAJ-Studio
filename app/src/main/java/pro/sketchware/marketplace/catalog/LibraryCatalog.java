58714ac23981039916468d4505ad6d7bccc9919b
luna <Khaled.hassansadik@gmail.com>
2026-08-05 15:35:59 +0300

    Library Version Picker

--- app/src/main/java/pro/sketchware/marketplace/catalog/LibraryCatalog.java
+++ app/src/main/java/pro/sketchware/marketplace/catalog/LibraryCatalog.java
@@ -85,16 +85,28 @@ public class LibraryCatalog {
         add(libraries, "coil", "Coil", "Image loading backed by Kotlin Coroutines", "io.coil-kt:coil:2.5.0", "Image Loading", "https://github.com/coil-kt/coil", R.drawable.ic_lib_coil, true);
         add(libraries, "picasso", "Picasso", "Powerful image downloading and caching", "com.squareup.picasso:picasso:2.8", "Image Loading", "https://github.com/square/picasso", R.drawable.ic_mtrl_palette);
         add(libraries, "fresco", "Fresco", "Manage images and their memory", "com.facebook.fresco:fresco:3.1.3", "Image Loading", "https://github.com/facebook/fresco", G);
+        add(libraries, "gpuimage", "GPUImage", "GPU-accelerated image filters", "jp.co.cyberagent.android:gpuimage:2.1.0", "Image Loading", "https://github.com/cats-oss/android-gpuimage", G);
+        add(libraries, "blurry", "Blurry", "Easy blur library for Android", "jp.wasabeef:blurry:4.0.1", "Image Loading", "https://github.com/wasabeef/Blurry", G);
+        add(libraries, "glide-transformations", "Glide Transformations", "Transformation library for Glide", "jp.wasabeef:glide-transformations:4.3.0", "Image Loading", "https://github.com/wasabeef/glide-transformations", G);
         
         // --- Animation ---
         add(libraries, "lottie", "Lottie", "Render After Effects animations natively", "com.airbnb.android:lottie:6.3.0", "Animation", "https://github.com/airbnb/lottie-android", R.drawable.ic_mtrl_animation, true);
         add(libraries, "shimmer", "Shimmer", "Shimmer loading effect for views", "com.facebook.shimmer:shimmer:0.5.0", "Animation", "https://github.com/facebook/shimmer-android", G);
+        add(libraries, "rebound", "Rebound", "Spring dynamics animation library", "com.facebook.rebound:rebound:0.3.8", "Animation", "https://github.com/facebook/rebound", G);
+        add(libraries, "konfetti", "Konfetti", "Lightweight confetti particle system", "nl.dionsegijn:konfetti-xml:2.0.4", "Animation", "https://github.com/DanielSegin/Konfetti", G);
+        add(libraries, "lottie-compose", "Lottie Compose", "Lottie for Jetpack Compose", "com.airbnb.android:lottie-compose:6.3.0", "Animation", "https://github.com/airbnb/lottie-android", R.drawable.ic_mtrl_animation);
         
         // --- Networking ---
         add(libraries, "retrofit", "Retrofit", "Type-safe HTTP client", "com.squareup.retrofit2:retrofit:2.9.0", "Networking", "https://github.com/square/retrofit", R.drawable.ic_lib_retrofit, true);
         add(libraries, "okhttp", "OkHttp", "Efficient HTTP & HTTP/2 client", "com.squareup.okhttp3:okhttp:4.12.0", "Networking", "https://github.com/square/okhttp", R.drawable.ic_lib_okhttp, true);
         add(libraries, "okhttp-logging", "OkHttp Logging", "Log HTTP requests and responses", "com.squareup.okhttp3:logging-interceptor:4.12.0", "Networking", "https://github.com/square/okhttp", R.drawable.ic_lib_okhttp);
         add(libraries, "volley", "Volley", "HTTP library for Android by Google", "com.android.volley:volley:1.2.1", "Networking", "https://github.com/google/volley", G);
+        add(libraries, "ktor", "Ktor", "Connected applications framework", "io.ktor:ktor-client-android:2.3.7", "Networking", "https://github.com/ktorio/ktor", G);
+        add(libraries, "apollo", "Apollo Kotlin", "GraphQL client for Kotlin and Java", "com.apollographql.apollo3:apollo-runtime:3.8.2", "Networking", "https://github.com/apollographql/apollo-kotlin", G);
+        add(libraries, "socket-io", "Socket.IO", "Real-time bidirectional event-based communication", "io.socket:socket.io-client:2.1.0", "Networking", "https://github.com/socketio/socket.io-client-java", G);
+        add(libraries, "jsoup", "Jsoup", "Java HTML parser", "org.jsoup:jsoup:1.17.2", "Networking", "https://github.com/jhy/jsoup", G);
+        add(libraries, "jackson", "Jackson", "Fast JSON processor", "com.fasterxml.jackson.core:jackson-databind:2.16.1", "Networking", "https://github.com/FasterXML/jackson", G);
+        add(libraries, "kotlinx-serialization", "Kotlinx Serialization", "Kotlin multiplatform serialization", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2", "Networking", "https://github.com/Kotlin/kotlinx.serialization", G);
         
         // --- JSON ---
         add(libraries, "gson", "Gson", "Java serialization/deserialization to JSON", "com.google.code.gson:gson:2.10.1", "JSON", "https://github.com/google/gson", G, true);
@@ -103,6 +115,12 @@ public class LibraryCatalog {
         // --- Database ---
         add(libraries, "room", "Room", "SQLite abstraction layer by AndroidX", "androidx.room:room-runtime:2.6.1", "Database", "https://developer.android.com/training/data-storage/room", R.drawable.ic_mtrl_database_added, true);
         add(libraries, "datastore", "DataStore", "Modern preferences & data storage", "androidx.datastore:datastore-preferences:1.1.1", "Database", "https://developer.android.com/topic/libraries/architecture/datastore", G);
+        add(libraries, "room-ktx", "Room KTX", "Kotlin extensions for Room", "androidx.room:room-ktx:2.6.1", "Database", "https://developer.android.com/training/data-storage/room", R.drawable.ic_mtrl_database_added);
+        add(libraries, "sqlite", "SQLite", "SQLite support library", "androidx.sqlite:sqlite:2.4.0", "Database", "https://developer.android.com/jetpack/androidx/releases/sqlite", G);
+        add(libraries, "objectbox", "ObjectBox", "High-performance NoSQL database", "io.objectbox:objectbox-android:3.7.1", "Database", "https://github.com/objectbox/objectbox-java", G);
+        add(libraries, "realm", "Realm", "Mobile database alternative to SQLite", "io.realm.kotlin:library-base:1.13.0", "Database", "https://github.com/realm/realm-kotlin", G);
+        add(libraries, "sqldelight", "SQLDelight", "Type-safe SQL from your SQL statements", "app.cash.sqldelight:android-driver:2.0.1", "Database", "https://github.com/cashapp/sqldelight", G);
+        add(libraries, "mmkv", "MMKV", "High-efficient key-value storage", "com.tencent:mmkv:1.3.2", "Database", "https://github.com/Tencent/MMKV", G);
         
         // --- UI ---
         add(libraries, "material", "Material Components", "Material Design components for Android", "com.google.android.material:material:1.11.0", "UI", "https://github.com/material-components/material-components-android", R.drawable.ic_mtrl_material3, true);
@@ -115,6 +133,21 @@ public class LibraryCatalog {
         add(libraries, "flexbox", "FlexboxLayout", "CSS flexbox layout for Android", "com.google.android.flexbox:flexbox:3.0.0", "UI", "https://github.com/google/flexbox-layout", G);
         add(libraries, "swiperefresh", "SwipeRefreshLayout", "Pull-to-refresh layout", "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0", "UI", "https://developer.android.com/jetpack/androidx/releases/swiperefreshlayout", G);
         add(libraries, "preference", "Preference", "Settings screens library", "androidx.preference:preference:1.2.1", "UI", "https://developer.android.com/jetpack/androidx/releases/preference", G);
+        add(libraries, "gridlayout", "GridLayout", "Grid-based layout", "androidx.gridlayout:gridlayout:1.0.0", "UI", "https://developer.android.com/jetpack/androidx/releases/gridlayout", G);
+        add(libraries, "transition", "Transition", "Animate layout changes", "androidx.transition:transition:1.5.0", "UI", "https://developer.android.com/jetpack/androidx/releases/transition", G);
+        add(libraries, "dynamicanimation", "DynamicAnimation", "Physics-based animations", "androidx.dynamicanimation:dynamicanimation:1.0.0", "UI", "https://developer.android.com/jetpack/androidx/releases/dynamicanimation", G);
+        add(libraries, "palette", "Palette", "Extract colors from images", "androidx.palette:palette:1.0.0", "UI", "https://developer.android.com/jetpack/androidx/releases/palette", G);
+        add(libraries, "emoji2", "Emoji2", "Modern emoji support", "androidx.emoji2:emoji2:1.4.0", "UI", "https://developer.android.com/jetpack/androidx/releases/emoji2", G);
+        add(libraries, "webkit", "WebKit", "WebView and web-related tools", "androidx.webkit:webkit:1.9.0", "UI", "https://developer.android.com/jetpack/androidx/releases/webkit", G);
+        add(libraries, "browser", "Browser", "Custom tabs and browser integration", "androidx.browser:browser:1.8.0", "UI", "https://developer.android.com/jetpack/androidx/releases/browser", G);
+        add(libraries, "drawerlayout", "DrawerLayout", "Navigation drawer widget", "androidx.drawerlayout:drawerlayout:1.2.0", "UI", "https://developer.android.com/jetpack/androidx/releases/drawerlayout", G);
+        add(libraries, "coordinatorlayout", "CoordinatorLayout", "Coordinate complex animations", "androidx.coordinatorlayout:coordinatorlayout:1.2.0", "UI", "https://developer.android.com/jetpack/androidx/releases/coordinatorlayout", G);
+        add(libraries, "slidingpanelayout", "SlidingPaneLayout", "Multi-pane layouts", "androidx.slidingpanelayout:slidingpanelayout:1.2.0", "UI", "https://developer.android.com/jetpack/androidx/releases/slidingpanelayout", G);
+        add(libraries, "multidex", "MultiDex", "Support for multiple DEX files", "androidx.multidex:multidex:2.0.1", "UI", "https://developer.android.com/studio/build/multidex", G);
+        add(libraries, "splashscreen", "Splash Screen", "Android 12+ splash screen API", "androidx.core:core-splashscreen:1.0.1", "UI", "https://developer.android.com/develop/ui/views/launch/splash-screen", G);
+        add(libraries, "fastadapter", "FastAdapter", "The bulletproof RecyclerView adapter", "com.mikepenz:fastadapter:5.7.0", "UI", "https://github.com/mikepenz/FastAdapter", G);
+        add(libraries, "epoxy", "Epoxy", "Build complex screens in a RecyclerView", "com.airbnb.android:epoxy:5.1.4", "UI", "https://github.com/airbnb/epoxy", G);
+        add(libraries, "brvah", "BRVAH", "Powerful RecyclerView adapter helper", "io.github.cymchad:BaseRecyclerViewAdapterHelper:4.0.1", "UI", "https://github.com/CymChad/BaseRecyclerViewAdapterHelper", G);
         
         // --- Architecture / Lifecycle ---
         add(libraries, "viewmodel", "ViewModel", "Lifecycle-aware view state holder", "androidx.lifecycle:lifecycle-viewmodel:2.7.0", "Architecture", "https://developer.android.com/topic/libraries/architecture/viewmodel", G);
@@ -124,9 +157,19 @@ public class LibraryCatalog {
         add(libraries, "activity-ktx", "Activity KTX", "Kotlin extensions for Activity", "androidx.activity:activity-ktx:1.9.0", "Architecture", "https://developer.android.com/jetpack/androidx/releases/activity", G);
         add(libraries, "paging", "Paging 3", "Load and display paged data", "androidx.paging:paging-runtime:3.3.0", "Architecture", "https://developer.android.com/topic/libraries/architecture/paging/v3-overview", G);
         add(libraries, "eventbus", "EventBus", "Publish/subscribe event bus", "org.greenrobot:eventbus:3.3.1", "Architecture", "https://github.com/greenrobot/EventBus", G);
+        add(libraries, "dagger", "Dagger", "Fast dependency injector for Java and Android", "com.google.dagger:dagger:2.50", "Architecture", "https://github.com/google/dagger", G);
+        add(libraries, "koin", "Koin", "Pragmatic lightweight dependency injection", "io.insert-koin:koin-android:3.5.3", "Architecture", "https://github.com/InsertKoinIO/koin", G);
+        add(libraries, "lifecycle-runtime", "Lifecycle Runtime", "Lifecycle runtime support", "androidx.lifecycle:lifecycle-runtime:2.7.0", "Architecture", "https://developer.android.com/jetpack/androidx/releases/lifecycle", G);
+        add(libraries, "lifecycle-process", "Lifecycle Process", "Process-level lifecycle support", "androidx.lifecycle:lifecycle-process:2.7.0", "Architecture", "https://developer.android.com/jetpack/androidx/releases/lifecycle", G);
+        add(libraries, "lifecycle-service", "Lifecycle Service", "Service-level lifecycle support", "androidx.lifecycle:lifecycle-service:2.7.0", "Architecture", "https://developer.android.com/jetpack/androidx/releases/lifecycle", G);
+        add(libraries, "startup", "App Startup", "Initialize components at app startup", "androidx.startup:startup-runtime:1.1.1", "Architecture", "https://developer.android.com/topic/libraries/app-startup", G);
+        add(libraries, "tracing", "Tracing", "Write trace events to the system trace buffer", "androidx.tracing:tracing:1.2.0", "Architecture", "https://developer.android.com/jetpack/androidx/releases/tracing", G);
+        add(libraries, "workmanager-ktx", "WorkManager KTX", "Kotlin extensions for WorkManager", "androidx.work:work-runtime-ktx:2.9.0", "Architecture", "https://developer.android.com/topic/libraries/architecture/workmanager", G);
         
         // --- Navigation ---
         add(libraries, "navigation", "Navigation", "In-app navigation framework", "androidx.navigation:navigation-fragment:2.7.7", "Navigation", "https://developer.android.com/guide/navigation", G, true);
+        add(libraries, "navigation-ui", "Navigation UI", "UI components for Navigation", "androidx.navigation:navigation-ui:2.7.7", "Navigation", "https://developer.android.com/guide/navigation", G);
+        add(libraries, "deeplinkdispatch", "DeepLinkDispatch", "Declarative deep link handling", "com.airbnb:deeplinkdispatch:6.1.0", "Navigation", "https://github.com/airbnb/DeepLinkDispatch", G);
         
         // --- Background / Concurrency ---
         add(libraries, "coroutines", "Coroutines", "Kotlin coroutines for Android", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3", "Background", "https://github.com/Kotlin/kotlinx.coroutines", G);
@@ -142,17 +185,28 @@ public class LibraryCatalog {
         
         // --- Media ---
         add(libraries, "media3", "Media3 ExoPlayer", "Modern media playback (ExoPlayer)", "androidx.media3:media3-exoplayer:1.3.1", "Media", "https://developer.android.com/guide/topics/media/media3", G);
+        add(libraries, "media3-ui", "Media3 UI", "UI components for Media3", "androidx.media3:media3-ui:1.3.1", "Media", "https://developer.android.com/guide/topics/media/media3", G);
+        add(libraries, "media3-session", "Media3 Session", "Media session support for Media3", "androidx.media3:media3-session:1.3.1", "Media", "https://developer.android.com/guide/topics/media/media3", G);
+        add(libraries, "media", "Media", "Legacy media-compat library", "androidx.media:media:1.7.0", "Media", "https://developer.android.com/jetpack/androidx/releases/media", G);
+        add(libraries, "ffmpeg", "FFmpeg Kit", "FFmpeg for Android", "com.arthenica:ffmpeg-kit-full:6.0-2", "Media", "https://github.com/arthenica/ffmpeg-kit", G);
+        add(libraries, "compressor", "Compressor", "Android image compression library", "id.zelory:compressor:3.0.1", "Media", "https://github.com/zetetic/android-database-sqlcipher", G);
         
         // --- Security ---
         add(libraries, "biometric", "Biometric", "Fingerprint & face authentication", "androidx.biometric:biometric:1.1.0", "Security", "https://developer.android.com/jetpack/androidx/releases/biometric", G);
         add(libraries, "security-crypto", "Security Crypto", "Encrypted SharedPreferences & files", "androidx.security:security-crypto:1.0.0", "Security", "https://developer.android.com/topic/security/data", G);
+        add(libraries, "tink", "Tink", "Multi-language, cross-platform crypto library", "com.google.crypto.tink:tink-android:1.12.0", "Security", "https://github.com/google/tink", G);
+        add(libraries, "sqlcipher", "SQLCipher", "Full database encryption for SQLite", "net.zetetic:android-database-sqlcipher:4.5.4", "Security", "https://github.com/sqlcipher/android-database-sqlcipher", G);
         
         // --- Debug ---
         add(libraries, "timber", "Timber", "Logger with a small, extensible API", "com.jakewharton.timber:timber:5.0.1", "Debug", "https://github.com/JakeWharton/timber", G);
         add(libraries, "leakcanary", "LeakCanary", "Memory leak detection for Android", "com.squareup.leakcanary:leakcanary-android:2.13", "Debug", "https://github.com/square/leakcanary", G);
+        add(libraries, "flipper", "Flipper", "Extensible mobile app debugger", "com.facebook.flipper:flipper:0.242.0", "Debug", "https://github.com/facebook/flipper", G);
+        add(libraries, "zxing-core", "ZXing Core", "Barcode image processing library", "com.google.zxing:core:3.5.2", "Debug", "https://github.com/zxing/zxing", G);
         
         // --- QR / Barcode ---
         add(libraries, "zxing", "ZXing Embedded", "Barcode & QR scanning (embedded)", "com.journeyapps:zxing-android-embedded:4.3.0", "QR & Barcode", "https://github.com/journeyapps/zxing-android-embedded", G);
+        add(libraries, "mlkit-barcode", "ML Kit Barcode Scanning", "On-device barcode scanning", "com.google.mlkit:barcode-scanning:17.2.0", "QR & Barcode", "https://developers.google.com/ml-kit/vision/barcode-scanning", G);
+        add(libraries, "code-scanner", "Code Scanner", "Google Play services code scanner", "com.google.android.gms:play-services-code-scanner:16.1.0", "QR & Barcode", "https://developers.google.com/android/guides/code-scanner", G);
         
         // --- Firebase ---
         add(libraries, "firebase-auth", "Firebase Auth", "Secure user sign-in", "com.google.firebase:firebase-auth:22.3.1", "Firebase", "https://firebase.google.com/docs/auth", R.drawable.ic_mtrl_firebase_auth, true);
